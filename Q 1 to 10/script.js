14167069dfbd62c004641d109d1dc2c480cd3f99
Vaghaniharshh <hvaghani89@gmail.com>
2025-08-26 14:56:54 +0530

    Q-4 update

--- Q 1 to 10/script.js
+++ Q 1 to 10/script.js
@@ -39,4 +39,12 @@
 
 // 4) JavaScript Program to Calculate the Area of a Triangle 
 
+{
+    let b = parseInt(prompt("Enter the base : "))
+    let h = parseInt(prompt("Enter the height :"))
 
+    let area = 0.5 * b * h
+
+    console.log("Area of triangle is : ", area)
+    
+}
